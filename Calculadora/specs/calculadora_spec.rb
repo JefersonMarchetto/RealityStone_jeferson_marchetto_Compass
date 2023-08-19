7c30d9ab9f284d7caf7780053e0bcb0dd51a982d
Jeferson <jefe.bolha@gmail.com>
2023-08-19 18:07:09 -0300

    calcuadora3

--- Calculadora/specs/calculadora_spec.rb
+++ Calculadora/specs/calculadora_spec.rb
@@ -19,4 +19,56 @@ describe 'Calculadora' do
   it 'deve subtrair dois numeros inteiros' do
     expect(calculadora.subtrai(5, 4)).to eq 1
   end
+
+  it 'deve subtrair qualquer numero com 0' do
+    expect(calculadora.subtrai(5, 0)).to eq 5
+  end
+
+  it 'deve subtrair um numero por ele mesmo' do
+    expect(calculadora.subtrai(5, 5)).to eq 0
+  end
+
+  # Multiplicação
+  it 'deve multiplicar dois numeros inteiros' do
+    expect(calculadora.multiplica(3, 4)).to eq 12
+  end
+
+  it 'deve multiplicar um numero por 0' do
+    expect(calculadora.multiplica(5, 0)).to eq 0
+  end
+
+  it 'deve multiplicar um numero por 1' do
+    expect(calculadora.multiplica(7, 1)).to eq 7
+  end
+
+  # Divisão
+  it 'deve dividir dois numeros inteiros' do
+    expect(calculadora.divide(10, 2)).to eq 5
+  end
+
+  it 'deve dividir um numero por 1' do
+    expect(calculadora.divide(9, 1)).to eq 9
+  end
+
+  it 'deve retornar mensagem de erro para divisão por zero' do
+    expect(calculadora.divide(10, 0)).to eq 'Erro: Divisão por zero!'
+  end
+   # Potência
+  it 'deve elevar um numero a uma potência' do
+    expect(calculadora.potencia(2, 3)).to eq 8
+  end
+
+  it 'deve elevar um numero a potência 0' do
+    expect(calculadora.potencia(5, 0)).to eq 1
+  end
+
+  # Radiciação
+  it 'deve calcular a raiz quadrada de um numero' do
+    expect(calculadora.raiz_quadrada(25)).to eq 5
+  end
+
+  it 'deve calcular a raiz quadrada de um numero negativo' do
+    expect(calculadora.raiz_quadrada(-9)).to eq 'Erro: Número negativo!'
+  end
+
 end
--- Calculadora/src/calculadora.rb
+++ Calculadora/src/calculadora.rb
@@ -6,4 +6,27 @@ class Calculadora
   def subtrai(a, b)
     a - b
   end
+
+  def multiplica (a, b)
+    a * b
+  end
+
+  def divide(a, b)
+    if b != 0
+      a / b
+    else
+      'Erro: Divisão por zero!'
+    end
+  end
+  def potencia(a, b)
+    a ** b
+  end
+
+  def raiz_quadrada(numero)
+    if numero >= 0
+      Math.sqrt(numero)
+    else
+      'Erro: Número negativo!'
+    end
+  end
 end
